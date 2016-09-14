4b4d216e2f7981bb183c51df10ec70183a307e29
Nikola Stefanov <stefanov.nm@gmail.com>
2016-09-14 20:20:49 +0300

    added filter, not working yet

--- app.js
+++ app.js
@@ -242,3 +242,4 @@ function convertDate(oldDate) {
     return newDate;
 }
 
+
---
+++ filter.js
@@ -0,0 +1,13 @@
+function filterByFirstDate() {
+    var firstFilter = document.getElementById("firstDate");
+    console.log(firstFilter.value);
+    
+
+}
+
+function filterBySecondDate() {
+    var secondFilter = document.getElementById("secondDate");
+    console.log(secondFilter.value);
+    
+
+}
